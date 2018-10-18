7f0576a72bf2ec33ae06d27a77307f3cf9889d5b
Heidijvr <43320863+Heidijvr@users.noreply.github.com>
2018-10-18 16:01:50 -0700

    back to square 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,24 @@
-document.onkeyup = function(event) {  
-    var userGuess = event.key; 
-
-function letterInput(event) {
-    console.log(event.type);
-    console.log(event.keyCode);
-    console.log(String.fromCharCode(event.keyCode)); 
-}
-
-var textAreaElement = document.getElementsByTagName('textarea') [0];
-    textAreaElement.addEventListener('keypress',eventHandler, false);
-    textAreaElement.addEventListener('keydown',eventHandler, false);
-    textAreaElement.addEventListener('keyup',eventHandler, false);
-
-    // Alerts the key the user pressed (userGuess).
-    alert("User guess: " + userGuess);
+var lettersArray = [];
+var allWords = ["Pluto", "Eeyore", "Spiderman", "Scrooge", "Homer", "Dopey", "Superman", "Stitch", "Animaniacs", "Popeye", "PorkyPig", "Cinderella", "Bart", "Goofy", "SpeedyGonzales"];
+var currentWord = "";
  
-    // Alerts the Computer's guess.
-    alert("Computer guess: " + computerGuess);        
-
-
-var word = "poespas".toLowerCase().split("");
-var displayedWord = "_".repeat(word.length).split("");
-
-
-guess = "l".toLowerCase();
-
-var matches = 0;
-
-for (var i = 0; i < word.length; i++) {
-// alert(word[i]);
-console.log(word[i])
-if (word[i] == guess) {
-    // alert("Ditsem!")
-    console.log("Heita!")
-    displayedWord[i] = guess;
-    matches++;
-}
+// Choose a random element from the array
+var getRandomWord = function(words) {
+  return words[Math.floor(Math.random() * words.length)];
 }
 
-console.log(displayedWord.join(""));
-console.log("Matches: " + matches );
+document.onkeyup = function(event) {
+  // Captures the key press, and saves it to a variable.
+  var letter = event.key;
 
-var wins = 0
-var lose = 0
+  console.log(letter);
+  lettersArray.push(letter);
+  document.getElementById("lettersGuessed").innerHTML = lettersArray.join(" ");
+};
 
-var userChoice = document.getElemnetById("user-choice");
-var userChoice = document.getElemnetById("cpu-choice");
-var userChoice = document.getElemnetById("wins");
-var userChoice = document.getElemnetById("lose");
+$(document).ready(function(){ 
+  currentWord = getRandomWord(allWords);
+  console.log(currentWord);
+  document.getElementById("wordToGuess").innerHTML = currentWord;
+ }) 
 
-let display = document.querySelector('.display');
-let guessQuerySelector = document.querySelector('#character');
-let textForm = document.querySelector('.textForm');
-
-var commonWords = [
-"Pluto", "Eeyore", "Spiderman", "ScroogeMcDuck", "MightyMouse", "CharlieBrown", "HomerSimpson", "BettyBoop", "PowerPuffGirls", "FredFlintstone", "Dopey", "Superman", "LinusvanPelt", "Stitch", "Animaniacs", "Popeye", "PorkyPig", "WoodyWoodpecker", "BartSimpson", "Goofy", "SpeedyGonzales"];
-
-
-// Grabbing Random Word
-var getRandomWord = function(array) {
-return array[Math.floor(Math.random() * array.length)];
-}
-
-var randomWord = getRandomWord(commonWords);
-console.log('randomWord', randomWord);
-
-// Function that submits the values
-textForm.addEventListener('submit', function(event) {
-event.preventDefault();
-
-var counter = 10;
-var triedCharacters = [];
-var correctCharacters = [];
-
-var guessWord = guessQuerySelector.value;
-var shorterWordlength = randomWord.length > guessWord.length ? guessWord.length : randomWord.length;
-
-console.log('guessWord', guessWord);
-
-for (i = 0; i < shorterWordlength; i++) {
- if (guessWord[i] === randomWord[i]) {
-   correctCharacters.push(guessWord[i])
-   console.log("correct " + correctCharacters)
- } else {
-   triedCharacters.push(guessWord[i])
-   console.log("incorrect " + triedCharacters)
- }
-}
-randomWord = getRandomWord(commonWords);
-console.log('randomWord', randomWord);
